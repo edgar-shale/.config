a97f3ff7ed50852838b90839729f951f2cc03be6
devalhales <stanly_mathai@condenast.com>
2026-04-21 13:10:17 +0530

    chore(git): ignore .bak files

--- fish/functions/cvn.fish
+++ fish/functions/cvn.fish
@@ -1,3 +1,3 @@
-function cvn --wraps='SECRETS_PROVIDER=DOTENV cargo run' --wraps='PORT=7878 SECRETS_PROVIDER=DOTENV cargo run' --wraps='PORT=7878 SECRETS_PROVIDER=DOTENV RUST_LOG=debug cargo run' --description 'alias cvn=PORT=7878 SECRETS_PROVIDER=DOTENV RUST_LOG=debug cargo run'
-    PORT=7878 SECRETS_PROVIDER=DOTENV RUST_LOG=debug cargo run $argv
+function cvn --wraps='SECRETS_PROVIDER=DOTENV cargo run' --wraps='PORT=7878 SECRETS_PROVIDER=DOTENV cargo run' --wraps='PORT=7878 SECRETS_PROVIDER=DOTENV RUST_LOG=debug cargo run' --wraps='SECRETS_PROVIDER=DOTENV RUST_LOG=debug cargo run' --description 'alias cvn=SECRETS_PROVIDER=DOTENV RUST_LOG=debug cargo run'
+    SECRETS_PROVIDER=DOTENV RUST_LOG=debug cargo run $argv
 end
